3b514dff9a0299baa6869a4ae62a3cb6b02a2139
ChenYiShan <295971920@qq.com>
2017-10-21 23:56:40 +0800

    购物车商品模型类

---
+++ src/cn/xiaoshan/model/Goodselement.java
@@ -0,0 +1,11 @@
+package cn.xiaoshan.model;
+	/**
+	 * 购物车商品模型类
+	 * @author www.xiaoshan2016.cn
+	 * @date 2017年10月21日
+	 */
+public class Goodselement {
+	public int ID; //商品ID
+	public float nowprice; //商品价格
+	public int number ; //数量
+}
